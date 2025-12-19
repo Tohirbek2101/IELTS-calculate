2c73c207782013aa6aa6747f68acae2aabcec80f
Tohirbek <isomiddinovtohirjon5@gmail.com>
2025-12-19 07:00:47 -0700

    Add project files.

---
+++ IELTS-calculate/Process.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IELTS_calculate
+{
+    public class Process
+    {
+        public double Listening;
+        public double Reading;
+        public double Writing;
+        public double Speaking;
+        public double result;
+       public double CalculateOveral(double listening,double reading, double writing, double speaking)
+        {
+            this.Listening = listening;
+            this.Reading = reading;
+            this.Writing = writing;
+            this.Speaking = speaking;
+            double overall = (Listening + Reading + Writing + Speaking)/4.0;
+            result = Math.Round(overall * 2) / 2;
+            return result;
+        }
+
+    }
+}
---
+++ IELTS-calculate/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Reflection.Metadata;
+
+namespace IELTS_calculate
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int shape = 1;
+            Console.WriteLine("Assalomu alaykum! Dasturimizga hush kelibsiz.");
+            do
+            {
+                Console.WriteLine("Ushbu dastur sizning IELTS imtihonidan olgan umumiy balingizni chiqarib beradi...");
+                Console.WriteLine("So'ralgan ma'lumotlarni kiritishingizni so'rab qolamiz...");
+               
+                Console.Write("Listening ballingizni kiriting: ");
+                double listening = Convert.ToDouble(Console.ReadLine());
+                
+                Console.Write("Reading ballingizni kiriting: ");
+                double reading = Convert.ToDouble(Console.ReadLine());
+                
+                Console.Write("Writing ballingizni kiriting: ");
+                double writing = Convert.ToDouble(Console.ReadLine());
+                
+                Console.Write("Speaking ballingizni kiriting: ");
+                double speaking = Convert.ToDouble(Console.ReadLine());
+                
+                Process process = new Process();
+                
+                process.CalculateOveral(listening, reading, writing, speaking);
+               
+                Console.WriteLine($"\nSizning IELTS bo‘yicha umumiy balingiz: {process.result}");
+                Console.WriteLine("Dasturdan foydalanilganingiz uchun rahmat!");
+                
+                Console.WriteLine("1. Dasturni davom ettirish");
+                Console.WriteLine("0. Dasturdan chiqish");
+                
+                shape = Convert.ToInt32(Console.ReadLine());
+            } while (shape != 0);
+        }
+    }
+}
